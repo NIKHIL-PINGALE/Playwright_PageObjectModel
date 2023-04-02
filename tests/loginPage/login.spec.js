659a95a6e67e3e416ebeec1c5d682f06778afa98
NIKHIL-PINGALE <nikhilpingale021@gmail.com>
2023-04-02 22:43:54 +0530

    1st commit

---
+++ tests/loginPage/login.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../../page-Objects/pages/loginPage';
+
+test('test', async ({ page }) => {
+
+  const Login = new LoginPage(page)
+
+  await Login.gotoLoginPage()
+  await Login.login('tomsmith', 'SuperSecretPassword!')
+
+ 
+});
